0f8767157f7ff52958c5e37a9c5752bdc3eb8ae5
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-02-17 14:31:23 -0800

    put comment on typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,12 @@
 const sentence = "hello there from lighthouse labs";
 let delay = 0;
+// delayed 50ms for each output
 for (const letter of sentence) {
   setTimeout(() => {
     process.stdout.write(letter);
   },delay += 500);
 }
+// we print '\n' at the end to avoid printing '%' at end of out sentence.
 setTimeout(() => {
   console.log('\n');
 },delay);
